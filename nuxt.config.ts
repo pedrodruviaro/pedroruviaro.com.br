b203b7596cd91b80f96f407dcceed1596b7344ab
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-10 13:42:39 -0300

    feat: favicon and fix nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     fontsDir: "/assets/fonts",
     overwriting: true,
     families: {
-      Raleway: true,
+      Raleway: [300, 400, 500, 600, 700, 800],
     },
   },
   site: {
