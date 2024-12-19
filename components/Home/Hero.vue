ab9bdee3f22a287161206c609325c8442f8c9955
pedrodruviaro <pedrodruviaro@gmail.com>
2024-12-18 21:42:14 -0300

    feat: change hero section content

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -17,37 +17,10 @@
     </BaseTitle>
     <div class="space-y-3">
       <p class="font-medium">
-        Desenvolvedor front-end apaixonado por tecnologia e aprendizado
-        contínuo, com foco no desenvolvimento front-end e, mais recentemente,
-        back-end. Tenho experiência em HTML, CSS, TypeScript, Vue e Nuxt, e
-        estou buscando abranger todo o ecossistema JavaScript e TypeScript.
-      </p>
-
-      <p class="font-medium">
-        Iniciei minha jornada em 2021, migrando da área de Química, e desde
-        então tenho me aprofundado cada vez mais no universo do desenvolvimento.
-        Gosto de estudar, ensinar e aprender coisas novas. Em 2024, comecei o
-        projeto
-        <NuxtLink
-          class="border-b inline-block border-dashed hover:opacity-80"
-          target="_blank"
-          rel="nofollow"
-          href="https://www.wire.art.br/"
-          >wire</NuxtLink
-        >, onde oferecemos serviços de desenvolvimento, design, e consultoria de
-        marketing e SEO para empresas e pessoas físicas.
-      </p>
-
-      <p class="font-medium">
-        Você pode me encontrar facilmente pelo
-        <NuxtLink
-          class="border-b inline-block border-dashed hover:opacity-80"
-          target="_blank"
-          rel="nofollow"
-          href="https://www.linkedin.com/in/pedroruviaro/"
-          >LinkedIn</NuxtLink
-        >
-        ou pelos links abaixo.
+        Desenvolvedor web com mais de três anos de experiência focado em
+        front-end, especialmente na stack Vue, Nuxt e TypeScript. Atualmente,
+        também trabalho com PHP e Laravel, expandindo minhas habilidades para o
+        back-end e projetos fullstack.
       </p>
     </div>
   </section>
