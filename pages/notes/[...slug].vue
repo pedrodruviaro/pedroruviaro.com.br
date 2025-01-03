631fb10d0a3281e7aaca8e37bd7d368394ad76ea
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-03 10:17:23 -0300

    feat: add post date and tag on single note page

--- pages/notes/[...slug].vue
+++ pages/notes/[...slug].vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { formatDate } from "~/utils/post/formatDate"
+</script>
 
 <template>
   <BaseContainer as="section">
@@ -6,6 +8,18 @@
 
     <div class="prose max-w-[100%]">
       <ContentDoc>
+        <template v-slot="{ doc }">
+          <div>
+            <div class="mb-6 lg:mb-8 flex gap-3 items-center flex-wrap">
+              <BaseTag>{{ doc.tag }}</BaseTag>
+              <p class="text-sm mb-0 mt-0 text-brand-black/80">
+                {{ formatDate(doc.date) }}
+              </p>
+            </div>
+            <ContentRenderer :value="doc" />
+          </div>
+        </template>
+
         <template #not-found>
           <PostsNotFound />
         </template>
