92ad36b45de21bfc8435c27711609255f85f6c8c
Pedro Henrique <pedrodruviaro@gmail.com>
2025-04-24 10:51:47 -0300

    feat: add banner back and add new styles to projects section

--- components/Home/Projects.vue
+++ components/Home/Projects.vue
@@ -3,13 +3,13 @@
 <template>
   <section>
     <div
-      class="border border-brand-black rounded-xl p-5 grid gap-5 md:p-8 md:grid-cols-[1fr_.5fr] md:items-center"
+      class="border border-brand-black rounded-xl p-5 md:grid md:gap-10 md:relative"
     >
-      <div>
+      <div class="md:max-w-[70%]">
         <BaseTitle
           size="sm"
           label="Conheça alguns de meus projetos"
-          class="mb-1"
+          class="mb-3"
         />
         <p>
           Explore o que eu ando produzinho no meu tempo livre. Caso queira saber
@@ -21,15 +21,28 @@
             >email</a
           >.
         </p>
-      </div>
 
-      <div class="md:grid md:justify-end">
         <NuxtLink
           to="/projects"
-          class="leading-snug bg-brand-accent rounded-lg border border-brand-black text-brand-black py-2 px-4 hover:bg-brand-accent/50 transition-all"
+          class="block max-w-max mt-6 leading-snug bg-brand-black rounded-lg text-brand-white py-2 px-4 hover:bg-brand-black/85 transition-all"
         >
           Explorar projetos
         </NuxtLink>
+      </div>
+
+      <div
+        class="hidden md:block absolute right-2.5 top-2.5 bottom-2.5 max-w-max"
+      >
+        <img
+          src="/images/projects-shape.svg"
+          alt=""
+          aria-hidden="true"
+          class="w-full h-full"
+          width="75"
+          height="149"
+          loading="lazy"
+          decoding="async"
+        />
       </div>
     </div>
   </section>
